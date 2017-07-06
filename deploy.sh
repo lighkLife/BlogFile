05acc36640892544567e97daf68fe162780900f5
KaiHao Yang <daoshibushi@gmail.com>
2017-07-06 13:16:31 +0800

    msg

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
  #!/bin/bash
- 
+msg= 
+if [ ! -n "$1"]; then
+    echo "-"
+else
+    msg= $1
+fi
 cd lighklife
 hexo clean
 hexo g
 hexo d
 cd ..
 git add --all
-git commit --all
+git commit -m msg
 git push
 
